6003c81aa3e5f75a3e716559b78658db1b7ff2a7
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-04-06 09:52:49 -0400

    Create .js

---
+++ .js
@@ -0,0 +1,20 @@
+/*jshint multistr:true */
+var text = "I've got doggies sleeping, Jacob Jacob";
+
+var myName = "Jacob";
+
+var hits = [];
+
+for(var i = 0; i < text.length; i++){
+    if (text[i] === myName[0]) {
+        for (var j = i;j < (i + myName.length); j++){
+            hits.push(text[j]);
+        }
+    }
+}
+
+if (hits.length === 0) {
+    console.log("Your name wasn't found!");
+} else {
+    console.log(hits);
+}
